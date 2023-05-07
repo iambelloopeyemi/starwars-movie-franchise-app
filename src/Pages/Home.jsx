60dd9c0a698b0522ecc8d87b813c0f21efe4d941
iambelloopeyemi <opeyemiemmanuelbello@gmail.com>
2023-05-07 15:14:24 +0100

    updated the UI

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import deathStar from "../assets/death_star.png";
+import MovieCard from "../components/MovieCard";
+
+export default function Home() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // FETCHED DATA FROM API USING AXIOS
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`https://swapi.dev/api/films`);
+        setData(response.data.results);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, []);
+  return (
+    <main className="relative">
+      <div className="fixed offset">
+        {loading && <img className="w-20 h-20 animate-spin-slow" src={deathStar} />}
+      </div>
+      <div>
+        {error && (
+          <div className="flex justify-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 h-6 text-yellow-300"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+              />
+            </svg>
+            <div className="text-yellow-300">{`${error}`}</div>
+          </div>
+        )}
+      </div>
+
+      <div className="px-28 mb-10">{data && <MovieCard data={data} />}</div>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        skyblue: "0 0 10px 2px rgb(81, 178, 243)",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(360deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
